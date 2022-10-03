d5b86c8f00f637301c789789fd129c2546d24c1e
jeonga-Son <jjeong0106@gmail.com>
2022-10-03 11:41:08 -0400

    Initial Commit

---
+++ uitest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  uitest
+//
+//  Created by jeonga Son on 2022/10/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+                
+        Text("Hello").padding()
+            .background(Color.blue)
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ uitest/uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  uitest
+//
+//  Created by jeonga Son on 2022/10/03.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
